a17b63ea438b79af9b3e96dd81a43600d8d87157
egboyguda <e.gboyguda@gmail.com>
2021-08-27 10:15:15 +0800

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.count = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textbox_add = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.search = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "cat",
+            "dog",
+            "mouse",
+            "ayam",
+            "yatot"});
+            this.comboBox2.Location = new System.Drawing.Point(94, 199);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.comboBox2.MouseLeave += new System.EventHandler(this.comboBox2_MouseLeave);
+            this.comboBox2.MouseHover += new System.EventHandler(this.comboBox2_MouseHover);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(61, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(60, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "INDEX NUMBER";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(62, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Selected Item";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(343, 360);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.search);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.count);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.comboBox2);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(335, 334);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Combo";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // count
+            // 
+            this.count.AutoSize = true;
+            this.count.Location = new System.Drawing.Point(53, 202);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(35, 13);
+            this.count.TabIndex = 8;
+            this.count.Text = "label4";
+            this.count.Click += new System.EventHandler(this.count_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(176, 235);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.textbox_add);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(335, 334);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Add Item";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Add Item";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(79, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(186, 23);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Item Name";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textbox_add
+            // 
+            this.textbox_add.Location = new System.Drawing.Point(50, 96);
+            this.textbox_add.Name = "textbox_add";
+            this.textbox_add.Size = new System.Drawing.Size(249, 20);
+            this.textbox_add.TabIndex = 0;
+            this.textbox_add.Enter += new System.EventHandler(this.textbox_add_Enter);
+            this.textbox_add.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textbox_add_KeyDown);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(60, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(191, 23);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Search Item";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(56, 155);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(190, 20);
+            this.search.TabIndex = 10;
+            this.search.KeyDown += new System.Windows.Forms.KeyEventHandler(this.search_KeyDown);
+            // 
+            // button4
+            // 
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(252, 155);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(21, 20);
+            this.button4.TabIndex = 11;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 398);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textbox_add;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label count;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox search;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.count.Text = this.comboBox2.Items.Count.ToString();
+        }
+
+        private void comboBox2_MouseHover(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Aqua;
+        }
+
+        private void comboBox2_MouseLeave(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.White;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.comboBox2.SelectedIndex == -1) {
+                MessageBox.Show("please select a value");
+                return;
+            }
+            this.textBox1.Text = this.comboBox2.SelectedIndex.ToString();
+            this.textBox2.Text = this.comboBox2.SelectedItem.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            addItem();
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.comboBox2.Items.Remove(this.comboBox2.SelectedItem);
+            this.count.Text = comboBox2.Items.Count.ToString();
+        }
+
+        private void count_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textbox_add_Enter(object sender, EventArgs e)
+        {
+          
+        }
+
+        public void addItem() {
+            if (this.textbox_add.Text == "") {
+                MessageBox.Show("enter a value first");
+                return;
+            }
+            this.comboBox2.Items.Add(this.textbox_add.Text);
+            this.textbox_add.ResetText();
+            MessageBox.Show("successfully added");
+            this.count.Text = comboBox2.Items.Count.ToString();
+        }
+
+        private void textbox_add_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) {
+                addItem();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            findVal();
+        }
+
+        private void search_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) {
+                findVal();
+            }
+        }
+
+        private void findVal() {
+
+            if (this.comboBox2.Items.Contains(this.search.Text))
+            {
+
+                this.comboBox2.Text = this.search.Text;
+                this.textBox1.Text = this.comboBox2.SelectedIndex.ToString();
+                this.textBox2.Text = this.comboBox2.SelectedItem.ToString();
+
+            }
+            else
+            {
+                MessageBox.Show("not found");
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
